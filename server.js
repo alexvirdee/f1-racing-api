ebb889cdc33969c3ea83165c715b3135dfbc8fbc
Alexinder Virdee <alex.virdee08@gmail.com>
2022-07-28 17:00:48 -0400

    Server successfully make api call to formula one api

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-const port = 3001;
+require('dotenv').config();
 
-app.get('/', (req, res) => res.send('Hello world'));
+const port = process.env.PORT || 3001;
+
+const f1key = process.env.F1_API_KEY;
+
+// GET request for remote image in node.js
+// axios({
+//     method: 'get',
+//     url: 'http://bit.ly/2mTM3nY',
+//     responseType: 'stream'
+//   })
+//     .then(function (response) {
+//       response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+//     });
+
+app.get('/', async (req, res, next) => {
+  try {
+    // Craft sport radar API URL
+    const url = `https://api.sportradar.us/formula1/trial/v2/en/competitors/sr:competitor:7135/profile.json?api_key=${f1key}`;
+
+    // Make request to API and forward the response
+    const response = await axios.get(url);
+
+    const data = res.json(response.data);
+
+    console.log('DATA: ', data);
+
+    return response;
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
